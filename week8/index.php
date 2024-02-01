473042daddc8e331cee59ae542ef8b76293b794c
Gian Sobrevinas <gsobrevinas@student.apc.edu.ph>
2024-02-01 18:41:40 +0800

    rename: contact > artworks

--- week8/index.php
+++ week8/index.php
@@ -135,7 +135,7 @@ section .wave::after {
   <li><a href="index.php">Home</a></li>
   <li><a href="./News.php">News</a></li>
   <li><img src="./Images/logo.webp" width="64px" height="64px" ></li>
-  <li><a href="./Archon.php">Contact</a></li>
+  <li><a href="./Archon.php">Artworks</a></li>
   <li ><a href="./About.php">About</a></li>
 </ul>
 </div>
